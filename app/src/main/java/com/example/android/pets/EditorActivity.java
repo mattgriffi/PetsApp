3134e8945e4cb6494baa26bbc317aadc1d92402b
mseffner <mseffner@gmail.com>
2017-07-12 06:23:13 -0700

    removed "delete" menu option from insert mode

--- app/src/main/java/com/example/android/pets/EditorActivity.java
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -104,6 +104,7 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
             getSupportLoaderManager().initLoader(0, null, this);
         } else {
             setTitle(R.string.editor_activity_title_new_pet);
+            invalidateOptionsMenu();
         }
     }
 
@@ -193,6 +194,16 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         // Inflate the menu options from the res/menu/menu_editor.xml file.
         // This adds menu items to the app bar.
         getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        if (uri == null) {
+            MenuItem menuItem = menu.findItem(R.id.action_delete);
+            menuItem.setVisible(false);
+        }
         return true;
     }
 
